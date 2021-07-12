8644973b0ce86477562ada73a1c8a3772ff93e4b
Chris3606 <cridley106@gmail.com>
2021-07-11 19:38:12 -0500

    Rewrote player controls component to support modifiers, and rewrote motion system.  Basic functionality mostly working.

--- TheSadRogue.Integration/Components/PlayerControlsComponent.cs
+++ TheSadRogue.Integration/Components/PlayerControlsComponent.cs
@@ -10,97 +10,285 @@ using SadRogue.Primitives;
 namespace SadRogue.Integration.Components
 {
     /// <summary>
+    /// Modifiers for key commands.
+    /// </summary>
+    public enum KeyModifiers
+    {
+        /// <summary>
+        /// Represents either the left or right shift key.
+        /// </summary>
+        Shift,
+        /// <summary>
+        /// Represents either the left or right ctrl key.
+        /// </summary>
+        Ctrl,
+        /// <summary>
+        /// Represents either the left or right alt key.
+        /// </summary>
+        Alt
+    };
+
+    /// <summary>
+    /// Represents an input state that can be bound to an action.
+    /// </summary>
+    public readonly struct ControlMapping : IEquatable<ControlMapping>, IMatchable<ControlMapping>
+    {
+        /// <summary>
+        /// Key for the input state.
+        /// </summary>
+        public readonly Keys Key;
+
+        /// <summary>
+        /// Whether or not a shift key must be pressed to satisfy the key state.
+        /// </summary>
+        public readonly bool RequiresShift;
+
+        /// <summary>
+        /// Whether or not a ctrl key must be pressed to satisfy the key state.
+        /// </summary>
+        public readonly bool RequiresCtrl;
+
+        /// <summary>
+        /// Whether or not an alt key must be pressed to satisfy the key state.
+        /// </summary>
+        public readonly bool RequiresAlt;
+
+        /// <summary>
+        /// Creates a new input state.
+        /// </summary>
+        /// <param name="key">Key for the input state.</param>
+        /// <param name="modifiers">Any required modifiers for the input state (SHIFT, CTRL, etc.)</param>
+        public ControlMapping(Keys key, params KeyModifiers[] modifiers)
+        {
+            Key = key;
+            RequiresCtrl = false;
+            RequiresAlt = false;
+            RequiresShift = false;
+
+            foreach (var modifier in modifiers)
+            {
+                switch (modifier)
+                {
+                    case KeyModifiers.Ctrl:
+                        RequiresCtrl = true;
+                        break;
+                    case KeyModifiers.Alt:
+                        RequiresAlt = true;
+                        break;
+                    case KeyModifiers.Shift:
+                        RequiresShift = true;
+                        break;
+                    default:
+                        throw new ArgumentException("Unsupported key modifier given.", nameof(modifiers));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implicitly converts a Keys enumeration value to an input state (with no modifiers), for convenience.
+        /// </summary>
+        /// <param name="key">Key to convert.</param>
+        /// <returns>An input state representing the key given with no modifiers.</returns>
+        public static implicit operator ControlMapping(Keys key) => new ControlMapping(key);
+
+        /// <summary>
+        /// True if the given keyboard state meets the modifier conditions for this input state.
+        /// </summary>
+        /// <remarks>
+        /// This function ensures that precisely the modifiers the input state specifies are true, and NO others.
+        /// </remarks>
+        /// <param name="keyboard">Keyboard state to check.</param>
+        /// <returns>True if the keyboard state meets the modifier conditions for this input state; false otherwise.</returns>
+        public bool ModiferConditionsMet(Keyboard keyboard)
+            => !RequiresCtrl || keyboard.IsKeyPressed(Keys.LeftControl) || keyboard.IsKeyPressed(Keys.RightControl) &&
+                !RequiresAlt || keyboard.IsKeyPressed(Keys.LeftAlt) || keyboard.IsKeyPressed(Keys.RightAlt) &&
+                !RequiresShift || keyboard.IsKeyPressed(Keys.LeftShift) || keyboard.IsKeyPressed(Keys.RightShift);
+
+        /// <summary>
+        /// True if the input state given precisely matches the current one in key and modifiers; false otherwise.
+        /// </summary>
+        /// <param name="other"/>
+        /// <returns/>
+        public bool Equals(ControlMapping other) => Matches(other);
+
+        /// <summary>
+        /// True if the given object is an input state that precisely matches the current one in key and modifiers;
+        /// false otherwise.
+        /// </summary>
+        /// <param name="obj"/>
+        /// <returns/>
+        public override bool Equals(object? obj) => obj is ControlMapping m && Matches(m);
+
+        /// <summary>
+        /// True if the input state given precisely matches the current one in key and modifiers; false otherwise.
+        /// </summary>
+        /// <param name="other"/>
+        /// <returns/>
+        public bool Matches(ControlMapping other)
+            => Key == other.Key && RequiresCtrl == other.RequiresCtrl && RequiresAlt == other.RequiresShift == other.RequiresShift;
+
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(Key, RequiresCtrl, RequiresAlt, RequiresShift);
+
+        /// <summary>
+        /// True if the two input states specified precisely match in key and modifiers; false otherwise.
+        /// </summary>
+        /// <param name="lhs"/>
+        /// <param name="rhs"/>
+        /// <returns/>
+        public static bool operator==(ControlMapping lhs, ControlMapping rhs) => lhs.Matches(rhs);
+
+        /// <summary>
+        /// True if the two input states differ at all in key or modifiers; false if they are equivalent.
+        /// </summary>
+        /// <param name="lhs"/>
+        /// <param name="rhs"/>
+        /// <returns/>
+        public static bool operator!=(ControlMapping lhs, ControlMapping rhs) => !lhs.Matches(rhs);
+    }
+
+    /// <summary>
     /// A basic component that moves a character based on keyboard input
     /// </summary>
     public class PlayerControlsComponent : RogueLikeComponentBase
     {
+
+        private readonly Dictionary<Keys, List<(ControlMapping control, Action action)>> _actions;
         /// <summary>
-        /// A mapping of Keys to an action that can be performed
+        /// A mapping of controls to an action to be performed.
         /// </summary>
-        public ReadOnlyDictionary<Keys, Action> Actions => _actions.AsReadOnly();
-        private readonly Dictionary<Keys, Action> _actions;
+        public ReadOnlyDictionary<Keys, List<(ControlMapping control, Action action)>> Actions => _actions.AsReadOnly();
+
+        private readonly Dictionary<Keys, List<(ControlMapping control, Direction direction)>> _motions;
         /// <summary>
-        /// A mapping of Keys to an action that can be performed
+        /// A mapping of controls to a direction to generate, which will be passed to the <see cref="MotionHandler"/>.
         /// </summary>
-        public ReadOnlyDictionary<Keys, Direction> Motions => _motions.AsReadOnly();
-        private readonly Dictionary<Keys, Direction> _motions;
+        public ReadOnlyDictionary<Keys, List<(ControlMapping control, Direction direction)>> Motions => _motions.AsReadOnly();
+
 
         /// <summary>
-        /// Creates a new component that controls it's parent entity via keystroke
+        /// Function to call in order to handle motions generated from bindings set up in <see cref="Motions"/>.
         /// </summary>
-        public PlayerControlsComponent() : base(false, false, false, true, 1)
-        {
-            _actions = new Dictionary<Keys, Action>();
-            _motions = new Dictionary<Keys, Direction>()
-            {
-                { Keys.Left, Direction.Left},
-                { Keys.Right, Direction.Right},
-                { Keys.Up, Direction.Up},
-                { Keys.Down, Direction.Down},
-            };
-        }
+        public readonly Action<Direction> MotionHandler;
 
         /// <summary>
-        /// Create a PlayerControlsComponent with the provided dictionary of keys to actions
+        /// Creates a new component that controls it's parent entity via keystroke.
         /// </summary>
-        /// <param name="actions">A dictionary of Keys to Actions</param>
-        public PlayerControlsComponent(Dictionary<Keys, Action> actions) : this()
+        /// <param name="motionHandler">
+        /// The handler to use to handle any key bindings in <see cref="Motions"/>.  If set to null, a default handler
+        /// will be used that simply sets the parent object's Position if possible.
+        /// </param>
+        public PlayerControlsComponent(Action<Direction>? motionHandler = null)
+            : base(false, false, false, true, 1)
         {
-            _actions = actions;
-        }
-        /// <summary>
-        /// Create a PlayerControlsComponent with the provided dictionary of keys to movement directions
-        /// </summary>
-        /// <param name="motions">A dictionary of Keys to Movement Directions</param>
-        public PlayerControlsComponent(Dictionary<Keys, Direction> motions) : this()
-        {
-            _motions = motions;
-        }
-        /// <summary>
-        /// Create a PlayerControlsComponent with the provided dictionary of keys to movement directions and keys to actions
-        /// </summary>
-        /// <param name="motions"></param>
-        /// <param name="actions"></param>
-        public PlayerControlsComponent(Dictionary<Keys, Direction> motions, Dictionary<Keys, Action> actions) : this()
-        {
-            _motions = motions;
-            _actions = actions;
+            // Initialize dictionaries
+            _actions = new Dictionary<Keys, List<(ControlMapping, Action)>>();
+            _motions = new Dictionary<Keys, List<(ControlMapping control, Direction direction)>>();
+
+            // Add default motion controls
+            AddMotion(Keys.Up, Direction.Up);
+            AddMotion(Keys.Right, Direction.Right);
+            AddMotion(Keys.Down, Direction.Down);
+            AddMotion(Keys.Left, Direction.Left);
+
+            // Add motion handler, or default if one was not specified
+            MotionHandler = motionHandler ?? DefaultMotionHandler;
         }
 
         /// <summary>
-        /// Adds a new action to the keystrokes being listened for
+        /// Adds a new action to the keystrokes being listened for.
         /// </summary>
-        /// <param name="key">The Key to listen for</param>
-        /// <param name="action">The action to perform when said key is pressed</param>
-        public void AddKeyCommand(Keys key, Action action)
+        /// <param name="control">The input state to listen for.</param>
+        /// <param name="action">The action to perform when said binding is pressed.</param>
+        public void AddKeyCommand(ControlMapping control, Action action)
         {
-            if (!_actions.ContainsKey(key))
-                _actions.Add(key, action);
+            // TODO: Verify exclusivity and uniqueness with itself and _motions?
+            if (!_actions.ContainsKey(control.Key))
+                _actions[control.Key] = new List<(ControlMapping control, Action action)>();
+
+            _actions[control.Key].Add((control, action));
         }
 
         /// <summary>
-        /// Removes a key command from the set we're listening for
+        /// Removes the binding for a key command.
         /// </summary>
-        /// <param name="key">The key to remove</param>
-        public void RemoveKeyCommand(Keys key)
+        /// <param name="key">The input state to remove.</param>
+        public void RemoveKeyCommand(ControlMapping key)
         {
-            if (_actions.ContainsKey(key))
-                _actions.Remove(key);
+            var idx = _actions[key.Key].FindIndex(tuple => tuple.control.Matches(key));
+            if (idx == -1) throw new ArgumentException("Specified command could not be found.", nameof(key));
+
+            _actions[key.Key].RemoveAt(idx);
+
+            if (_actions[key.Key].Count == 0)
+                _actions.Remove(key.Key);
+        }
+
+        /// <summary>
+        /// Adds a new motion generating keybinding.
+        /// </summary>
+        /// <param name="control">Input state to use for generating motion.</param>
+        /// <param name="direction">Direction to generate when input state is detected.</param>
+        public void AddMotion(ControlMapping control, Direction direction)
+        {
+            // TODO: Verify exclusivity and uniqueness with itself and _actions?
+            if (!_motions.ContainsKey(control.Key))
+                _motions[control.Key] = new List<(ControlMapping control, Direction direction)>();
+
+            _motions[control.Key].Add((control, direction));
+        }
+
+        /// <summary>
+        /// Removes the binding for a motion.
+        /// </summary>
+        /// <param name="key">The input state to remove.</param>
+        public void RemoveMotion(ControlMapping key)
+        {
+            var idx = _motions[key.Key].FindIndex(tuple => tuple.control.Matches(key));
+            if (idx == -1) throw new ArgumentException("Specified command could not be found.", nameof(key));
+
+            _motions[key.Key].RemoveAt(idx);
+
+            if (_motions[key.Key].Count == 0)
+                _motions.Remove(key.Key);
         }
 
         /// <inheritdoc />
         public override void ProcessKeyboard(IScreenObject host, Keyboard keyboard, out bool handled)
         {
-            foreach (var motion in Motions)
-                if (keyboard.IsKeyPressed(motion.Key))
-                    if(Parent!.CanMoveIn(motion.Value))
-                        Parent!.Position += motion.Value;
+            // Map any keys pressed to appropriate key bindings
+            foreach (var key in keyboard.KeysPressed)
+            {
+                if (_motions.ContainsKey(key.Key))
+                {
+                    foreach (var (control, direction) in _motions[key.Key])
+                        if (control.ModiferConditionsMet(keyboard))
+                        {
+                            MotionHandler(direction);
+                            handled = true;
+                            return;
+                        }
+                }
 
-            foreach (KeyValuePair<Keys,Action> action in Actions)
-                if (keyboard.IsKeyPressed(action.Key))
-                    action.Value();
+                if (_actions.ContainsKey(key.Key))
+                {
+                    foreach (var (control, action) in _actions[key.Key])
+                        if (control.ModiferConditionsMet(keyboard))
+                        {
+                            action();
+                            handled = true;
+                            return;
+                        }
+                }
+            }
 
-            handled = true;
+            handled = false;
+        }
+
+        private void DefaultMotionHandler(Direction direction)
+        {
+            if(Parent!.CanMoveIn(direction))
+                Parent!.Position += direction;
         }
     }
 }
