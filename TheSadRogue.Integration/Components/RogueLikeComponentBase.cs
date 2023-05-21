160209bd6212d08cd607d72e6f37111a2032eddb
Chris3606 <cridley106@gmail.com>
2023-05-20 22:07:57 -0500

    Updated documentation on KeybindingsComponent to reflect its ability to be used on any IScreenObject.  Functionally removed default implementation of the MotionHandler (it is now a blank implementation).  RogueLikeComponentBase now stores its component of type "object", and the IObjectWithComponents type restriction has been removed from RogueLikeComponentBase<T>.
    These changes enable the KeybindingsComponent to be used with any IScreenObject, without losing functionality related to the Parent field.

--- TheSadRogue.Integration/Components/RogueLikeComponentBase.cs
+++ TheSadRogue.Integration/Components/RogueLikeComponentBase.cs
@@ -19,14 +19,13 @@ namespace SadRogue.Integration.Components
     /// The intended use is for you to derive from this class and then override the methods you need.
     ///
     /// If adding this component to a <see cref="RogueLikeEntity"/>, <see cref="Maps.RogueLikeMap"/>, or anything else
-    /// with an "AllComponents" field, you should attach this component to the AllComponents field; it will automatically
+    /// with an "AllComponents" field, you should attach this component to the AllComponents field ONLY; it will automatically
     /// be attached to SadConsole's components fields as needed.  If your object only has a SadComponents field, you can instead
     /// attach it directly there.  In either case, the Parent field will be updated accordingly.
     /// </remarks>
     [PublicAPI]
-    public abstract class RogueLikeComponentBase : RogueLikeComponentBase<IObjectWithComponents>
+    public abstract class RogueLikeComponentBase : RogueLikeComponentBase<object>
     {
-
         /// <summary>
         /// Creates a new component.
         /// </summary>
@@ -48,7 +47,7 @@ namespace SadRogue.Integration.Components
     /// <typeparam name="TParent">Type of object this component must be attached to.</typeparam>
     [PublicAPI]
     public class RogueLikeComponentBase<TParent> : ParentAwareComponentBase<TParent>, IComponent, ISortedComponent
-        where TParent : class, IObjectWithComponents
+        where TParent : class
     {
         /// <summary>
         /// The Order in which the components are processed. Lower is earlier.
@@ -107,8 +106,8 @@ namespace SadRogue.Integration.Components
         public virtual void Render(IScreenObject host, TimeSpan delta) { }
 
         /// <summary>
-        /// Hook up an event handler to run this when the component is added to an entity.  Ensure you
-        /// call the base implementation of this class if you override it; its functionality is required
+        /// Hook up an event handler to run this when the component is added to an object.  Ensure you
+        /// call the base implementation of this function if you override it; its functionality is required
         /// for this object's Parent field to be set correctly.
         /// </summary>
         /// <param name="host">The "Parent" if you're using RogueLikeEntity</param>
@@ -123,7 +122,9 @@ namespace SadRogue.Integration.Components
         }
 
         /// <summary>
-        /// Hook up an event handler to run this when the component is removed from an entity
+        /// Hook up an event handler to run this when the component is removed from an object.  Ensure you
+        /// call the base implementation of this function if you override it; its functionality is required
+        /// for this object's Parent field to be set correctly.
         /// </summary>
         /// <param name="host">The "Parent" if you're using RogueLikeEntity</param>
         public virtual void OnRemoved(IScreenObject host)
